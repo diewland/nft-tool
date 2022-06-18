439005debdeb8abacc2168e366bce58dc49b24b2
diewland <diewland@gmail.com>
2022-06-18 10:55:35 +0700

    revise code, add Apetimism

--- app.js
+++ app.js
@@ -1,65 +1,98 @@
 // screen var
+let web3 = null;
+let cur_provider = null;
 let cur_contract = null;
+let cur_ctr_addr = null;
 
-// UX
+// update version
+$("sub.version").html(VERSION);
+
+// log
+function l(msg) { $('#div-msg').append(msg + '<br>'); }
+function lx() { $('#div-msg').html(''); }
+
+// freeze/unfreeze
 function freeze(bool=true) {
   $('#sel-project').attr('disabled', bool);
   $('#ipt-token-id').attr('disabled', bool);
   $('#btn-download').attr('disabled', bool);
   if (bool) {
+    lx();
     $('#btn-download').addClass('spin');
-    $('#div-msg').html('');
     $('#img-preview').hide();
   }
   else {
+    lx();
     $('#btn-download').removeClass('spin');
-    $('#div-msg').html('');
     $('#img-preview').show();
   }
 }
 let unfreeze = () => freeze(false);
-freeze();
 
-// bind download button
+// download
+$('#img-preview').on('load', unfreeze);
 $('#btn-download').click(_ => {
-  let token_id = $('#ipt-token-id').val().trim();
   if (!cur_contract) return;
+  let token_id = $('#ipt-token-id').val().trim();
   if (!token_id) return;
   download(token_id);
 });
-
-// download
 function download(token_id) {
   freeze();
-  $('#div-msg').append('1. find meta-data...<br>');
+  download_op(token_id);
+}
+function download_op(token_id) {
+  l('1. find meta-data...');
   cur_contract.methods.tokenURI(token_id).call().then(r => {
     // https://ipfs.io/ipfs/QmZ2eKEiuV1UKxgsLrPUK9JXhvLhtbetxUwZEHRiqUaHq4/3356.json
     let json_url = 'https://ipfs.io/ipfs/' + r.split('ipfs://')[1];
-    $('#div-msg').append('2. read meta-data...<br>');
-    $.getJSON(json_url, info => {
+    l('2. read meta-data...');
+    q(json_url, info => {
       // {"image":"ipfs://QmeR1DwxMTZQPWSH2CSgNeAWHmXztsPek4SQHKtSZHg7XU","name":"Bored Town #3356","description":"Bored Town is a collection of 5555 Bored Town Monsters living on the Optimism blockchain. As an open-source brand (aka CC0), Bored Town holders have the chance to create whatever they put their mind to, both for personal and commercial purposes.","external_url":"https://quixotic.io/collection/boredtown","attributes":[{"trait_type":"Background","value":"Dark Gray"},{"trait_type":"Accessory","value":"White Mandala"},{"trait_type":"Body","value":"Sweater With Thorns"},{"trait_type":"Head","value":"Sponky"}],"compiler":"nft-inator.com"}
       // https://nftscan.mypinata.cloud/ipfs/QmeR1DwxMTZQPWSH2CSgNeAWHmXztsPek4SQHKtSZHg7XU
       let img_url = 'https://nftscan.mypinata.cloud/ipfs/' + info.image.split('ipfs://')[1];
-      $('#div-msg').append('3. load image...<br>');
+      l('3. load image...');
       $('#img-preview').attr('src', img_url);
     })
   });
 }
-$('#img-preview').on('load', unfreeze);
 
-// init web3
-let web3 = new Web3(PROVIDER_ALCHEMY);
+// JSON
+function q(url, callback) {
+  $.getJSON(url, data => callback(data));
+  // TODO add exception
+}
 
-// get current contract TODO update contract when change project
-let contract_addr = PROJECT_INFO[1].contract_addr;
-let contract_url = API_OP_CONTRACT + contract_addr;
-$.getJSON(contract_url, function (data) {
-    let contractABI = JSON.parse(data.result);
-    if (contractABI != ''){
-        cur_contract = new web3.eth.Contract(contractABI);
-        cur_contract.options.address = contract_addr;
+// bind project combo
+function update_contract(new_provider, new_ctr_addr) {
+  if (new_provider != cur_provider) {
+    l("update web3 provider..");
+    web3 = new Web3(new_provider);
+    cur_provider = new_provider;
+  }
+  if (new_ctr_addr != cur_ctr_addr) {
+    cur_ctr_addr = new_ctr_addr;
+    let contract_url = API_OP_CONTRACT + new_ctr_addr;
+    freeze();
+    l("switch contract..");
+    q(contract_url, function (data) {
+        let contractABI = JSON.parse(data.result);
+        if (contractABI != ''){
+            cur_contract = new web3.eth.Contract(contractABI);
+            cur_contract.options.address = new_ctr_addr;
+        } else {
+          alert('contractABI not found');
+        }
         unfreeze();
-    } else {
-      alert('Sorry, Please try again later.'); // TODO update text
-    }
+    });
+  }
+}
+$('#sel-project').change(evt => {
+  let proj_no = $(evt.target).val();
+  let info = PROJECT_INFO[proj_no];
+  if (!info) return;
+  let new_provider = info.provider;
+  let new_ctr_addr = info.contract_addr;
+  update_contract(new_provider, new_ctr_addr);
 });
+$('#sel-project').change();
--- config.js
+++ config.js
@@ -1,13 +1,22 @@
+// version
+const VERSION = "v1.0.0"
+
 // provider
-const PROVIDER_ALCHEMY = "https://opt-mainnet.g.alchemy.com/v2/MnmlgcGeD8FPWiy_0SHlubv1htTHIB1g";
+const PROVIDER_OP = "https://opt-mainnet.g.alchemy.com/v2/MnmlgcGeD8FPWiy_0SHlubv1htTHIB1g";
 
 // API
 const API_OP_CONTRACT = 'https://api-optimistic.etherscan.io/api?module=contract&action=getabi&apikey=A7YUEGDPZD2DD2G784BNDKK1WZBBQP7D4X&address=';
 
 // project info
 const PROJECT_INFO = {
-  // bored-town, optimism
+  // Bored Town, optimism
   '1': {
+    provider: PROVIDER_OP,
     contract_addr: '0x8E56343adAFA62DaC9C9A8ac8c742851B0fb8b03',
+  },
+  // Apetimism, optimism
+  '2': {
+    provider: PROVIDER_OP,
+    contract_addr: '0x51E5426eDE4e2d4c2586371372313B5782387222',
   },
 };
