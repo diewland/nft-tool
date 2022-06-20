52514e185c3ed0d776f9283811167c91dede99ed
diewland <diewland@gmail.com>
2022-06-20 23:46:50 +0700

    support qs

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ let cur_provider = null;
 let cur_contract = null;
 let cur_ctr_addr = null;
 
+// query string
+function get_qs(k) {
+  var half = location.search.split(k + '=')[1];
+  return half !== undefined ? decodeURIComponent(half.split('&')[0]) : null;
+}
+let qs_project = get_qs('project');
+let qs_token_id = get_qs('id');
+
 // update version
 $("sub.version").html(VERSION);
 
@@ -46,7 +54,8 @@ function download_op(token_id) {
   cur_contract.methods.tokenURI(token_id).call().then(r => {
     // https://ipfs.io/ipfs/QmZ2eKEiuV1UKxgsLrPUK9JXhvLhtbetxUwZEHRiqUaHq4/3356.json
     let json_url = 'https://ipfs.io/ipfs/' + r.split('ipfs://')[1];
-    l('2. reading meta-data...');
+    let refresh_url = './index.html?project=' + $('#sel-project').val().trim() + '&id=' + token_id;
+    l(`2. reading meta-data... <a href='${refresh_url}'>refresh</a>`);
     console.log(json_url);
     query(json_url, info => {
       // {"image":"ipfs://QmeR1DwxMTZQPWSH2CSgNeAWHmXztsPek4SQHKtSZHg7XU","name":"Bored Town #3356","description":"Bored Town is a collection of 5555 Bored Town Monsters living on the Optimism blockchain. As an open-source brand (aka CC0), Bored Town holders have the chance to create whatever they put their mind to, both for personal and commercial purposes.","external_url":"https://quixotic.io/collection/boredtown","attributes":[{"trait_type":"Background","value":"Dark Gray"},{"trait_type":"Accessory","value":"White Mandala"},{"trait_type":"Body","value":"Sweater With Thorns"},{"trait_type":"Head","value":"Sponky"}],"compiler":"nft-inator.com"}
@@ -54,7 +63,7 @@ function download_op(token_id) {
       // let img_url = 'https://nftscan.mypinata.cloud/ipfs/' + info.image.split('ipfs://')[1];
       // https://quixotic.infura-ipfs.io/ipfs/QmbKa8WixDNeZCsxjeAchgzC8UqiAq9YYrtHx8cgxLzRe6
       let img_url = 'https://quixotic.infura-ipfs.io/ipfs/' + info.image.split('ipfs://')[1];
-      l(`3. loading image...<br>If nothing happens for 1 minute, <a href='${img_url}'>click here</a>`);
+      l(`3. loading image... or <a href='${img_url}'>open directly</a>`);
       console.log(info);
       console.log(img_url);
       $('#img-preview').attr('src', img_url);
@@ -83,15 +92,21 @@ function update_contract(new_provider, new_ctr_addr) {
     freeze();
     l("switch contract..");
     query(url, data => {
+        unfreeze();
         let contractABI = JSON.parse(data.result);
-        if (contractABI != ''){
-            cur_contract = new web3.eth.Contract(contractABI);
-            cur_contract.options.address = new_ctr_addr;
-            console.log('contract', cur_contract);
-        } else {
+        if (contractABI) {
+          cur_contract = new web3.eth.Contract(contractABI);
+          cur_contract.options.address = new_ctr_addr;
+          console.log('contract', cur_contract);
+          if (qs_token_id) {
+            $('#ipt-token-id').val(qs_token_id);
+            $('#btn-download').click();
+            qs_token_id = null;
+          }
+        }
+        else {
           alert('contractABI not found');
         }
-        unfreeze();
     });
     cur_ctr_addr = new_ctr_addr;
   }
@@ -104,4 +119,5 @@ $('#sel-project').change(evt => {
   let new_ctr_addr = info.contract_addr;
   update_contract(new_provider, new_ctr_addr);
 });
+if (qs_project) $('#sel-project').val(qs_project);
 $('#sel-project').change();
