89c70ae45c5db427fcfc667a4fe14bf684105fad
diewland <diewland@gmail.com>
2022-07-03 13:05:09 +0700

    dev

--- nerdape/json/download_json.sh
+++ nerdape/json/download_json.sh
@@ -1,5 +1,8 @@
-# https://ipfs.io/ipfs/QmU8nj3AGqcW4ww7QywkRNRFLc7zvKa1GjMLVKWWdHoyvv/{0-3332}
+# https://natoboram.gitlab.io/public-gateway-cacher/
+# 20220701 https://ipfs.io/ipfs/QmU8nj3AGqcW4ww7QywkRNRFLc7zvKa1GjMLVKWWdHoyvv/{0-1081}
+# 20220702 https://ipfs.best-practice.se/ipfs/QmPeKHhGizYmTerkVMTJ87zUvFo5ZikfJ7Z3GwffzHWvBn/{1082-1300}
 for (( i=$1; i<=$2; i++ )); do
-  url="https://ipfs.io/ipfs/QmU8nj3AGqcW4ww7QywkRNRFLc7zvKa1GjMLVKWWdHoyvv/$i"
+  #url="https://ipfs.io/ipfs/QmPeKHhGizYmTerkVMTJ87zUvFo5ZikfJ7Z3GwffzHWvBn/$i"
+  url="https://ipfs.best-practice.se/ipfs/QmPeKHhGizYmTerkVMTJ87zUvFo5ZikfJ7Z3GwffzHWvBn/$i"
   wget --no-cache $url
 done
