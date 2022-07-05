31a8ae49db40374edf4240cc51920789d548f4ef
diewland <diewland@gmail.com>
2022-07-05 23:57:23 +0700

    add nerd-ape data 2022/06/05

--- nerdape/json/download_json.sh
+++ nerdape/json/download_json.sh
@@ -3,12 +3,9 @@
 # 20220702 https://ipfs.best-practice.se/ipfs/QmPeKHhGizYmTerkVMTJ87zUvFo5ZikfJ7Z3GwffzHWvBn/{1082-1300}
 # 20220703 https://cloudflare-ipfs.com/ipfs/QmSu1ALW1N9RnRuCSbigRADTqsukiRfzdkGxTNbMy72vWj/{1301-1342}
 # 20220704 https://cloudflare-ipfs.com/ipfs/QmPZpo1AY18TR3KM42HP9iwKZ1GwJ6GovdsruRugbniGJD/{1343-1357}
-
+# 20220705 https://ipfs.io/ipfs/QmbkNPgjhfj7B3qTAHVVBf6LauiFU4jxtQp9aBusbXCBeX/{1358-1376}
 
 for (( i=$1; i<=$2; i++ )); do
-  #url="https://ipfs.io/ipfs/QmPeKHhGizYmTerkVMTJ87zUvFo5ZikfJ7Z3GwffzHWvBn/$i"
-  #url="https://ipfs.best-practice.se/ipfs/QmPeKHhGizYmTerkVMTJ87zUvFo5ZikfJ7Z3GwffzHWvBn/$i"
-  #url="https://cloudflare-ipfs.com/ipfs/QmSu1ALW1N9RnRuCSbigRADTqsukiRfzdkGxTNbMy72vWj/$i"
-  url="https://cloudflare-ipfs.com/ipfs/QmPZpo1AY18TR3KM42HP9iwKZ1GwJ6GovdsruRugbniGJD/$i"
+  url="https://ipfs.io/ipfs/QmbkNPgjhfj7B3qTAHVVBf6LauiFU4jxtQp9aBusbXCBeX/$i"
   wget --no-cache $url
 done
