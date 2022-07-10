0d51794a57eeda777e017334352f7d0f3442b088
diewland <diewland@gmail.com>
2022-07-10 21:28:50 +0700

    add ape 2022/07/10 data

--- nerdape/json/download_json.sh
+++ nerdape/json/download_json.sh
@@ -7,8 +7,9 @@
 # 20220706 https://cloudflare-ipfs.com/ipfs/QmUFTMRBZSg75g6eMfERCfKCQbvjvHEkr2rWdipGjqveZj/{1377-1377}
 # 20220708 https://cloudflare-ipfs.com/ipfs/Qmeo52knfLsM2TevgvSqYngsxnq4tiSeVGY6dgA6JhYqMd/{1378-1381}
 # 20220709 https://cloudflare-ipfs.com/ipfs/QmUizFR1jMb2F851RT5x8EE82Cry1BwuHgpvuD4LgAaLxJ/{1382-1393}
+# 20220710 https://cloudflare-ipfs.com/ipfs/QmQ5rfacYVjmeNdX2hWZsXTZrTkxhmXRs6vMvXemYMPx2R/{1394-1401}
 
 for (( i=$1; i<=$2; i++ )); do
-  url="https://cloudflare-ipfs.com/ipfs/QmUizFR1jMb2F851RT5x8EE82Cry1BwuHgpvuD4LgAaLxJ/$i"
+  url="https://cloudflare-ipfs.com/ipfs/QmQ5rfacYVjmeNdX2hWZsXTZrTkxhmXRs6vMvXemYMPx2R/$i"
   wget --no-cache $url
 done
