2d36c8b313a0bf442d4bb529994c2da05a2d7ddf
diewland <diewland@gmail.com>
2022-07-11 08:58:18 +0700

    move ranking to other project

--- holo-mask-blue/json/download_json.sh
+++
@@ -1,7 +0,0 @@
-# https://api.speedboat.studio/HoloMaskBlue-00/uri/4444
-# 0-4444
-
-for (( i=$1; i<=$2; i++ )); do
-  url="https://api.speedboat.studio/HoloMaskBlue-00/uri/$i"
-  wget --no-cache $url
-done
--- holo-mask-bw/json/download_json.sh
+++
@@ -1,7 +0,0 @@
-# https://api.speedboat.studio/Holo-Mask--White--Black/uri/0
-# 0-4444
-
-for (( i=$1; i<=$2; i++ )); do
-  url="https://api.speedboat.studio/Holo-Mask--White--Black/uri/$i"
-  wget --no-cache $url
-done
